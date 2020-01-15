bfb7df1a1460db4784da7a93323c172c3f598428
koheiy1 <59187820+koheiy1@users.noreply.github.com>
2020-01-15 16:43:46 +0900

    initial commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -43,7 +43,7 @@ $(function(){
             message.user_name +
           `</div>` +
           `<div class="upper-message__date">` +
-            message.created_at +
+            message.date +
           `</div>` +
         `</div>` +
         `<div class="lower-message">` +
