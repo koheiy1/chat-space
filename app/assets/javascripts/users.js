84ea6d763241dac7342205d62d35d073f50c270c
koheiy1 <59187820+koheiy1@users.noreply.github.com>
2020-01-15 17:14:42 +0900

    initial commit

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -63,5 +63,10 @@ $(function() {
     addDeleteUser(userName, userId);
     addMember(userId);
   });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
+  });
 
 });
